36648768f24219a173da0ba26db4fecd29585037
PrathakGarg <202051144@iiitvadodara.ac.in>
2021-08-06 20:53:37 +0530

    Added basic auth and database backend

---
+++ restaurant/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Cuisines(models.Model):
+    cuisine_name = models.CharField(default=None, max_length=50)
+
+    def __str__(self):
+        return f'cuisine_name: {self.cuisine_name}'
+
+
+class Restaurant(models.Model):
+    restaurant_id = models.BigAutoField(verbose_name='Restaurant ID', primary_key=True)
+    restaurant_name = models.CharField(null=False, verbose_name='Restaurant Name', max_length=100)
+    country = models.IntegerField(default=1, verbose_name='Country ID')
+    city = models.CharField(null=False, verbose_name='City Name', max_length=100)
+    address = models.CharField(null=False, verbose_name='Address', max_length=255)
+    locality = models.CharField(null=False, verbose_name='Locality', max_length=255)
+    locality_verbose = models.CharField(null=False, verbose_name='Locality Landmarks', max_length=255)
+    longitude = models.FloatField(validators=[MaxValueValidator(-180), MinValueValidator(180)])
+    latitude = models.FloatField(validators=[MaxValueValidator(-85.05), MinValueValidator(85.05)])
+    avg_cost = models.IntegerField(verbose_name='Average cost for two')
+    currency = models.CharField(default='Indian Rupees(Rs.)', max_length=50)
+    cuisines = models.ManyToManyField(Cuisines)
+    has_table_booking = models.BooleanField(verbose_name='Allow Table Booking')
+    is_delivering_now = models.BooleanField(verbose_name='Delivering now')
+    rating = models.FloatField(verbose_name='Rating', validators=[MaxValueValidator(5), MinValueValidator(0)],
+                               default=0)
+    votes = models.IntegerField(verbose_name='Number of votes', default=0)
+    price_range = models.IntegerField(verbose_name='Price Range', default=2,
+                                      validators=[MaxValueValidator(4), MinValueValidator(1)])
+
+    def __str__(self):
+        return f'restaurant_id: {self.restaurant_id}, restaurant_name: {self.restaurant_name}'
