f77f41d517c18cf9ab76958010135e1aa853231f
Anubhav Kushwaha <k04kushwaha@gmail.com>
2021-08-08 19:59:28 +0530

    res

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import test_invoice, home
+from .views import test_invoice, home, restaurant_page
 
 urlpatterns = [
     path('test', test_invoice),
     path('', home, name='home'),
+    path('restaurant/<int:restaurant_id>', restaurant_page, name='restaurant'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+
+from restaurant.models import Restaurant
 from .models import Order
 from .utils import export_invoice_pdf
 from .utils import search
@@ -28,3 +31,12 @@ def home(request):
     data.update({'restaurants': rest.values()})
     print(data)
     return render(request, 'home.html', data)
+
+
+def restaurant_page(request, restaurant_id=1):
+    query = Restaurant.objects.filter(restaurant_id=restaurant_id)
+    if not query.exists():
+        return HttpResponse('404')
+    restaurant = query[0]
+
+
